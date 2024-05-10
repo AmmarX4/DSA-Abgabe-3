4025b2ee0a6ce896dc204211ceb5488f57768217
AmmarX4 <ammarhassnou@gmail.com>
2024-05-11 00:31:35 +0200

    Implemented InOrderIterator and PreOrderIterator
    testing needed, still have to implement PostOrderIterator and LevelOrderIterator...

--- src/main/java/de/unistuttgart/dsass2024/ex03/p5/BinarySearchTree.java
+++ src/main/java/de/unistuttgart/dsass2024/ex03/p5/BinarySearchTree.java
@@ -1,9 +1,6 @@
 package de.unistuttgart.dsass2024.ex03.p5;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchTreeIterable<T> {
 
@@ -45,6 +42,117 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 
     @Override
     public Iterator<T> iterator(TreeTraversalType traversalType) {
-        return null;
+
+        return switch (traversalType) {
+            case INORDER: new InOrderIterator();
+            case PREORDER: new PreOrderIterator();
+            case POSTORDER: new PostOrderIterator();
+            case LEVELORDER: new LevelOrderIterator();
+
+        };
+
+    }
+
+    private class InOrderIterator implements Iterator<T> {
+
+        private final Stack<IBinaryTreeNode<T>> stack = new Stack<>();
+
+        // Go to the deepest leftChild from the start
+        public InOrderIterator() {
+            IBinaryTreeNode<T> currentNode = root;
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.getLeftChild();
+            }
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("this tree has no more Elements");
+            }
+
+            IBinaryTreeNode<T> node = stack.pop();
+            T nodeValue = node.getValue();
+            IBinaryTreeNode<T> rightChild = node.getRightChild();
+
+            while (rightChild != null) {
+                stack.push(node.getRightChild());
+                rightChild = rightChild.getLeftChild();
+
+            }
+
+            return nodeValue;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("this method is not supported !");
+        }
+
+    }
+
+
+    private class PreOrderIterator implements Iterator<T> {
+
+        private Stack<IBinaryTreeNode<T>> stack = new Stack<>();
+
+        public PreOrderIterator() {
+            if (root != null) {
+                stack.push(root);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public T next() {
+
+            IBinaryTreeNode<T> node = stack.pop();
+            T nodeValue = node.getValue();
+
+            if (node.getRightChild() != null) {
+                stack.push(node.getRightChild());
+            }
+            if (node.getLeftChild() != null) {
+                stack.push(node.getLeftChild());
+            }
+
+            return nodeValue;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("this method is not supported !");
+        }
+    }
+
+
+    private class PostOrderIterator implements Iterator<T> {
+
+        private Stack<IBinaryTreeNode<T>> stack = new Stack<>();
+
+        public PostOrderIterator() {
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public T next() {}
+
+        public void remove() {
+            throw new UnsupportedOperationException("this method is not supported !");
+        }
     }
 }
